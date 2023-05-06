06c32ad72c62ccb7af62eb68eab14f56cc86f712
DiegoMonutti <diego_monutti@outlook.com>
2023-05-06 13:36:30 -0300

    Exercitando Orientação a Objetos

--- Mod 28 - Orientação a Objetos/agencias_banco.py
+++ Mod 28 - Orientação a Objetos/agencias_banco.py
@@ -17,7 +17,7 @@ class Agencia:
             print(f'O valor do caixa está ok. Caixa atual R${self.caixa:,.2f}')
     
     def emprestar_dinheiro(self, valor, cpf, juros):
-        if caixa > valor:
+        if self.caixa > valor:
             self.emprestimos.append((valor, cpf, juros))
         else:
             print('Empréstimo impossível de ser realizado. Valor não disponível em caixa.')
@@ -27,19 +27,37 @@ class Agencia:
 
 #Agência Virtual:
 class AgenciaVirtual(Agencia):
+
     def __init__(self, site, telefone, cnpj):
-        super().__init__(telefone, cnpj, 1000)
         self.site = site
+        super().__init__(telefone, cnpj, 1000)
         self.caixa = 1000000
+        self.caixa_paypal = 0
+    
+    def depositar_paypal(self, valor):
+        self.caixa -= valor
+        self.caixa_paypal += valor
+    
+    def sacar_paypal(self, valor):
+        self.caixa_paypal -= valor
+        self.caixa += valor
 
 #Agência Comum:
 class AgenciaComum(Agencia):
+
     def __init__(self, telefone, cnpj):
         super().__init__(telefone, cnpj, numero=randint(1001,9999))
         self.caixa = 1000000
 
 #Agência Premium:
 class AgenciaPremium(Agencia):
-    def __init__(self, telefone, cnpj, numero):
+
+    def __init__(self, telefone, cnpj):
         super().__init__(telefone, cnpj, numero=randint(1001,9999))
         self.caixa = 10000000
+    
+    def adicionar_cliente(self, nome, cpf, patrimonio):
+        if patrimonio > 1000000:
+            super().adicionar_cliente(nome, cpf, patrimonio)
+        else:
+            print('O Cliente não tem o patrimônio mínimo necessário para entrar na Agência Premium')
